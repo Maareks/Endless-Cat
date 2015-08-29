4f288bad000060ab3a7e1cebb55942c0578cbf79
Maareks <Maareks@wp.pl>
2015-08-29 14:35:01 +0200

    Unity 5.1.3 Added Animation to ambulans, sort project.

---
+++ Assets/Scripts/DEBUG_LogMessage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DEBUG_LogMessage : MonoBehaviour {
+	public string m_Message = "Empty Message";
+	private Animation anim;
+	public void LogMessage(){
+		Debug.Log (m_Message);
+	}
+	public void Animm(){
+		anim = gameObject.GetComponent<Animation> ();
+		anim.Play ();
+	}
+}
+
---
+++ Assets/Scripts/Event Triggers/Event_PlayerNear.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Event_PlayerNear : MonoBehaviour {
+	public float m_PlayerNearDistance;
+	public UnityEvent m_OnPlayerNear;
+
+	Transform Player{
+		get{
+			if(s_Player == null)
+			{
+				s_Player = GameObject.FindGameObjectWithTag("Player").transform;
+			}
+			return s_Player;
+		}
+	}
+
+	static Transform s_Player;
+
+	State m_State = State.Far;
+
+
+	public void ResetState(){
+		m_State = State.Far;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (m_State == State.Far) {
+			if((transform.position - Player.position).magnitude <= m_PlayerNearDistance)
+			{
+				m_OnPlayerNear.Invoke();
+				m_State = State.Near;
+			}
+		}
+	}
+
+	enum State{
+		Far,
+		Near
+	}
+}
